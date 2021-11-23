d9e682ec74b4f48bbd7e0a73aeec5191af79ded3
Ivan <ivanxintang@gmail.com>
2021-11-22 17:07:56 -0800

    Add queries for assignments_per_day, average_completion_time, busy_days, large_cohorts, low_average_completion_time, total_assignment_submission

---
+++ 3_queries_group-by/low_average_completion_time.sql
@@ -0,0 +1,10 @@
+SELECT students.name as student, 
+AVG(assignment_submissions.duration) as average_assignment_duration,
+AVG(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration;
