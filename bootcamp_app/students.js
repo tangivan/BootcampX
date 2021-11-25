32e0cabc98033210735efa47b1a1d23a4bdaa1a9
Ivan <ivanxintang@gmail.com>
2021-11-24 20:00:12 -0800

    Updated students.js and teachers.js with parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,14 +11,17 @@ pool.connect(() => {
   console.log("connected to database");
 });
 
-const month = process.argv[2];
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${month}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const month = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${month}%`, limit];
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,16 +11,18 @@ pool.connect(() => {
   console.log("connected to database");
 });
 
-const month = process.argv[2];
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${month || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+const month = process.argv[2] || 'JUL02';
+const values = [month];
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
